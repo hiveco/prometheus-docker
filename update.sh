9bb0cd9fb0e5da23d88c3a5ce2143176c0c981e6
Danny Bendelman <danny@hivecollective.co>
2018-04-29 12:00:23 -0400

    Autocommit on update

--- update.sh
+++ update.sh
@@ -9,6 +9,9 @@ sed -i "s#?PROMETHEUS_VERSION?#$current_branch#g" Dockerfile.generated
 if [ ! -f Dockerfile ] || ! cmp -s Dockerfile.generated Dockerfile; then
     rm Dockerfile 2>/dev/null || true
     mv Dockerfile.generated Dockerfile
+    git reset
+    git add Dockerfile
+    git commit -m "Updated version to $current_branch"
 else
     rm Dockerfile.generated
 fi
